98d47b621ebb2faf126b6324b2cb83d33a26ba8e
waritboonmasiri <warit.boonmasiri@gmail.com>
2026-02-12 22:27:44 +1100

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/thai-election69-fraud/',
+  base: '/thai-election69-fraud-isn-t/',
   plugins: [react(), tailwindcss()],
 })
